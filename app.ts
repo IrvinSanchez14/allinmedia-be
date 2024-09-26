4f21de154a412b23af613bee5f48c28c10c17f94
Irvin Sanchez <irvinsanchez@SVGNWK6MNWWX.local>
2024-09-26 11:51:32 -0600

    last changes

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import taskRouter from './src/routes/tasksRoutes'
 
 //For env File 
@@ -7,6 +8,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.use(express.json());
 
